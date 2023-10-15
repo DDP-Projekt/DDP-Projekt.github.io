43b745bc8b07fcee7b2f0dfe6c603b4387b878ed
NotLe0n <notle0n75@gmail.com>
2023-10-15 13:06:22 +0200

    download button, playground instead of img

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,33 @@
-//fetch("https://api.github.com/repos/DDP-Projekt/Kompilierer/releases/latest")
-//.then(x => x.json())
-//.then(x => console.log(x))
 
-//document.getElementById('dl-text')
+
 
 let cIndex = 1;
 const maxC = 3;
-//document.addEventListener('DOMContentLoaded', () => {
-//	
-//})
+document.addEventListener('DOMContentLoaded', async () => {
+	const latestRelease = await fetch("https://api.github.com/repos/DDP-Projekt/Kompilierer/releases/latest")
+		.then(x => x.json() )
+	console.log(latestRelease)
+
+	if (window.navigator.platform.includes("Win")) {
+		for (const asset of latestRelease.assets) {
+			if (asset.name.includes("win") && !asset.name.includes("mingw")) {
+				document.getElementById('dl-btn').onclick = () => open(asset.browser_download_url)
+				document.getElementById('dl-text').innerText = asset.name;
+			}
+		}
+	}
+	else if (window.navigator.platform.includes("Linux")) {
+		for (const asset of latestRelease.assets) {
+			if (asset.name.includes("linux")) {
+				document.getElementById('dl-btn').onclick = () => open(asset.browser_download_url)
+				document.getElementById('dl-text').innerText = asset.name;
+			}
+		}
+	}
+	else {
+		document.getElementById('dl-text').innerText = "OS not supported";
+	}
+})
 
 function nextC() {
 	document.getElementById('c'+cIndex).classList.toggle('hidden')
