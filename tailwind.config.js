9e0449ff9590d52e2059bbe373ec04c495afb762
Leon Gies <notle0n75@gmail.com>
2023-10-29 19:39:44 +0000

    help2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["/static/*.html", "/static/*.js"],
+	content: ["/static/*.html", "/static/js/*.js"],
 	theme: {
 	  extend: {},
 	},
